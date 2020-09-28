2d7651fbbb7b781e2ea802d2c85a5cd3579c9924
Priscillia Labourg <labourg1u@etu.univ-lorraine.fr>
2020-09-28 12:06:03 +0200

    Initialisation

---
+++ src/log_lib/print_log.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<FirstOrganizedCMakeConfig.h>
+#include<log_lib/print_log.h>
+
+void hello_world() {
+	std::cout 
+	<< "Hello World!\n" 
+	<< std::endl;
+}
+
+void print_version() 
+{ 
+	std::cout << "Project version is "
+		<< FirstOrganizedCMake_VERSION_MAJOR
+		<< "."
+		<< FirstOrganizedCMake_VERSION_MINOR
+		<< std::endl;
+
+}
